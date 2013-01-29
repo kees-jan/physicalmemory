99ab113908d9ea6fe0236347dcb0ac910429420c
Kees-Jan Dijkzeul <kees-jan.dijkzeul@iae.nl>
2013-01-29 17:06:46 +0100

    ..

--- physicalmemory.c
+++ physicalmemory.c
@@ -1,20 +1,3 @@
-/*
- * Simple - REALLY simple memory mapping demonstration.
- *
- * Copyright (C) 2001 Alessandro Rubini and Jonathan Corbet
- * Copyright (C) 2001 O'Reilly & Associates
- *
- * The source code in this file can be freely used, adapted,
- * and redistributed in source or binary form, so long as an
- * acknowledgment appears in derived source files.  The citation
- * should list that the code comes from the book "Linux Device
- * Drivers" by Alessandro Rubini and Jonathan Corbet, published
- * by O'Reilly & Associates.   No warranty is attached;
- * we cannot take responsibility for errors or fitness for use.
- *
- * $Id: simple.c,v 1.12 2005/01/31 16:15:31 rubini Exp $
- */
-
 // #include <linux/config.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -32,26 +15,26 @@
 
 #include <linux/device.h>
 
-static int simple_major = 0;
-module_param(simple_major, int, 0);
+static int physicalmemory_major = 0;
+module_param(physicalmemory_major, int, 0);
 MODULE_AUTHOR("Jonathan Corbet");
 MODULE_LICENSE("Dual BSD/GPL");
 
 /*
  * Open the device; in fact, there's nothing to do here.
  */
-static int simple_open (struct inode *inode, struct file *filp)
+static int physicalmemory_open (struct inode *inode, struct file *filp)
 {
-	return 0;
+  return 0;
 }
 
 
 /*
  * Closing is just as simpler.
  */
-static int simple_release(struct inode *inode, struct file *filp)
+static int physicalmemory_release(struct inode *inode, struct file *filp)
 {
-	return 0;
+  return 0;
 }
 
 
@@ -60,15 +43,15 @@ static int simple_release(struct inode *inode, struct file *filp)
  * Common VMA ops.
  */
 
-void simple_vma_open(struct vm_area_struct *vma)
+void physicalmemory_vma_open(struct vm_area_struct *vma)
 {
-	printk(KERN_NOTICE "Simple VMA open, virt %lx, phys %lx\n",
-			vma->vm_start, vma->vm_pgoff << PAGE_SHIFT);
+  printk(KERN_NOTICE "Physicalmemory VMA open, virt %lx, phys %lx\n",
+         vma->vm_start, vma->vm_pgoff << PAGE_SHIFT);
 }
 
-void simple_vma_close(struct vm_area_struct *vma)
+void physicalmemory_vma_close(struct vm_area_struct *vma)
 {
-	printk(KERN_NOTICE "Simple VMA close.\n");
+  printk(KERN_NOTICE "Physicalmemory VMA close.\n");
 }
 
 
@@ -77,21 +60,21 @@ void simple_vma_close(struct vm_area_struct *vma)
  * from drivers/char/mem.c.
  */
 
-static struct vm_operations_struct simple_remap_vm_ops = {
-	.open =  simple_vma_open,
-	.close = simple_vma_close,
+static struct vm_operations_struct physicalmemory_remap_vm_ops = {
+  .open =  physicalmemory_vma_open,
+  .close = physicalmemory_vma_close,
 };
 
-static int simple_remap_mmap(struct file *filp, struct vm_area_struct *vma)
+static int physicalmemory_remap_mmap(struct file *filp, struct vm_area_struct *vma)
 {
-	if (remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff,
-			    vma->vm_end - vma->vm_start,
-			    vma->vm_page_prot))
-		return -EAGAIN;
+  if (remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff,
+                      vma->vm_end - vma->vm_start,
+                      vma->vm_page_prot))
+    return -EAGAIN;
 
-	vma->vm_ops = &simple_remap_vm_ops;
-	simple_vma_open(vma);
-	return 0;
+  vma->vm_ops = &physicalmemory_remap_vm_ops;
+  physicalmemory_vma_open(vma);
+  return 0;
 }
 
 
@@ -99,18 +82,18 @@ static int simple_remap_mmap(struct file *filp, struct vm_area_struct *vma)
 /*
  * Set up the cdev structure for a device.
  */
-static void simple_setup_cdev(struct cdev *dev, int minor,
-		struct file_operations *fops)
+static void physicalmemory_setup_cdev(struct cdev *dev, int minor,
+                                      struct file_operations *fops)
 {
-	int err, devno = MKDEV(simple_major, minor);
+  int err, devno = MKDEV(physicalmemory_major, minor);
     
-	cdev_init(dev, fops);
-	dev->owner = THIS_MODULE;
-	dev->ops = fops;
-	err = cdev_add (dev, devno, 1);
-	/* Fail gracefully if need be */
-	if (err)
-		printk (KERN_NOTICE "Error %d adding simple%d", err, minor);
+  cdev_init(dev, fops);
+  dev->owner = THIS_MODULE;
+  dev->ops = fops;
+  err = cdev_add (dev, devno, 1);
+  /* Fail gracefully if need be */
+  if (err)
+    printk (KERN_NOTICE "Error %d adding physicalmemory%d", err, minor);
 }
 
 
@@ -118,62 +101,65 @@ static void simple_setup_cdev(struct cdev *dev, int minor,
  * Our various sub-devices.
  */
 /* Device 0 uses remap_pfn_range */
-static struct file_operations simple_remap_ops = {
-	.owner   = THIS_MODULE,
-	.open    = simple_open,
-	.release = simple_release,
-	.mmap    = simple_remap_mmap,
+static struct file_operations physicalmemory_remap_ops = {
+  .owner   = THIS_MODULE,
+  .open    = physicalmemory_open,
+  .release = physicalmemory_release,
+  .mmap    = physicalmemory_remap_mmap,
 };
 
-#define MAX_SIMPLE_DEV 1
+#define MAX_PHYSICALMEMORY_DEV 1
 
 #if 0
-static struct file_operations *simple_fops[MAX_SIMPLE_DEV] = {
-	&simple_remap_ops,
+static struct file_operations *physicalmemory_fops[MAX_PHYSICALMEMORY_DEV] = {
+  &physicalmemory_remap_ops,
 };
 #endif
 
 /*
- * We export two simple devices.  There's no need for us to maintain any
+ * We export two physicalmemory devices.  There's no need for us to maintain any
  * special housekeeping info, so we just deal with raw cdevs.
  */
-static struct cdev SimpleDevs[MAX_SIMPLE_DEV];
+static struct cdev PhysicalmemoryDevs[MAX_PHYSICALMEMORY_DEV];
 
 /*
  * Module housekeeping.
  */
-static int simple_init(void)
+static int physicalmemory_init(void)
 {
-	int result;
-	dev_t dev = MKDEV(simple_major, 0);
+  int result;
+  dev_t dev = MKDEV(physicalmemory_major, 0);
 
-	/* Figure out our device number. */
-	if (simple_major)
-		result = register_chrdev_region(dev, 2, "simple");
-	else {
-		result = alloc_chrdev_region(&dev, 0, 2, "simple");
-		simple_major = MAJOR(dev);
-	}
-	if (result < 0) {
-		printk(KERN_WARNING "simple: unable to get major %d\n", simple_major);
-		return result;
-	}
-	if (simple_major == 0)
-		simple_major = result;
+  printk(KERN_NOTICE "PhysicalMemory Init\n");
 
-	/* Now set up two cdevs. */
-	simple_setup_cdev(SimpleDevs, 0, &simple_remap_ops);
-	return 0;
+  /* Figure out our device number. */
+  if (physicalmemory_major)
+    result = register_chrdev_region(dev, 2, "physicalmemory");
+  else {
+    result = alloc_chrdev_region(&dev, 0, 2, "physicalmemory");
+    physicalmemory_major = MAJOR(dev);
+  }
+  if (result < 0) {
+    printk(KERN_WARNING "physicalmemory: unable to get major %d\n", physicalmemory_major);
+    return result;
+  }
+  if (physicalmemory_major == 0)
+    physicalmemory_major = result;
+
+  /* Now set up two cdevs. */
+  physicalmemory_setup_cdev(PhysicalmemoryDevs, 0, &physicalmemory_remap_ops);
+  return 0;
 }
 
 
-static void simple_cleanup(void)
+static void physicalmemory_cleanup(void)
 {
-	cdev_del(SimpleDevs);
-	cdev_del(SimpleDevs + 1);
-	unregister_chrdev_region(MKDEV(simple_major, 0), 2);
+  printk(KERN_NOTICE "PhysicalMemory Cleanup\n");
+  cdev_del(PhysicalmemoryDevs);
+  cdev_del(PhysicalmemoryDevs + 1);
+  unregister_chrdev_region(MKDEV(physicalmemory_major, 0), 2);
 }
 
 
-module_init(simple_init);
-module_exit(simple_cleanup);
+module_init(physicalmemory_init);
+module_exit(physicalmemory_cleanup);
